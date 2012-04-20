339507236b5599f8b1afcc746d0de9059c54cd7e
Ketema Harris <ketema@midnightoilconsulting.com>
2012-04-20 01:37:04 -0400

    First release of li3_jade everything works but here are some notes:
    The basic set up was to register a new media type with li3 which the
    bootstrap file does.  Then create the compiler and renderer classes.
    I used a two step process to get the php to be evaled: the template is
    cached and passed to jade parser, the resulting php is written to a
    cached file as well and that is what is added to the output buffer.

--- extensions/adapter/template/view/Jade.php
+++ extensions/adapter/template/view/Jade.php
@@ -2,31 +2,100 @@
 
 namespace li3_jade\extensions\adapter\template\view;
 
-use Jade as Renderer;
+use \jade\Everzet\Jade\Jade as Renderer;
+use \jade\Everzet\Jade\Parser;
+use \jade\Everzet\Jade\Dumper\PHPDumper;
+use \jade\Everzet\Jade\Visitor\AutotagsVisitor;
+use \jade\Everzet\Jade\Filter\CDATAFilter;
+use \jade\Everzet\Jade\Filter\PHPFilter;
+use \jade\Everzet\Jade\Filter\CSSFilter;
+use \jade\Everzet\Jade\Filter\JavaScriptFilter;
+use \jade\Everzet\Jade\Lexer\Lexer;
 use lithium\core\Libraries;
 
 class Jade extends \lithium\template\view\adapter\File
 {
     public function render( $template, $data = array(), array $options = array() )
     {
-        $this->_context = $options['context'] + $this->_context;
-        $this->_data = (array) $data + $this->_vars;
-        $renderer = new Renderer();
-        return $renderer->render(file_get_contents($template), $this->_context + $this->_data);
+        $defaults = array('context' => array());
+		$options += $defaults;
+
+		$this->_context = $options['context'] + $this->_context;
+		$this->_data = (array) $data + $this->_vars;
+		$template__ = $template;
+		unset($options, $template, $defaults, $data);
+
+		if ($this->_config['extract']) {
+			extract($this->_data, EXTR_OVERWRITE);
+		} elseif ($this->_view) {
+			extract((array) $this->_view->outputFilters, EXTR_OVERWRITE);
+		}
+
+        $compiledJade = $this->compile($template__);
+		ob_start();
+		include $compiledJade;
+		return ob_get_clean();
     }
 
     /**
-     * Returns a template file name
+     * Returns a jade compiled cached template file name
      *
      * @param string $type
      * @param array $params
      * @return string
      */
-    public function template($type, array $params) {
-        $params += array('library' => true);
-        $params['library'] = Libraries::get($params['library'], 'path');
-        return $this->_paths($type, $params);
+    public function compile($file, array $options = array() )
+    {
+        $cachePath = Libraries::get(true, 'resources') . '/tmp/cache/templates';
+		$defaults = array('path' => $cachePath, 'fallback' => true);
+		$options += $defaults;
+        $dumper = new PHPDumper();
+        $dumper->registerVisitor('tag', new AutotagsVisitor());
+        $dumper->registerFilter('javascript', new JavaScriptFilter());
+        $dumper->registerFilter('cdata', new CDATAFilter());
+        $dumper->registerFilter('php', new PHPFilter());
+        $dumper->registerFilter('style', new CSSFilter());
+        $parser = new Parser(new Lexer());
+        $renderer = new Renderer($parser, $dumper);
+
+		$stats = stat($file);
+		$dir = dirname($file);
+		$oname = basename(dirname($dir)) . '_' . basename($dir) . '_' . basename($file, '.php');
+		$template = "template_{$oname}_{$stats['ino']}_{$stats['mtime']}_{$stats['size']}.php";
+		$template = "{$options['path']}/{$template}";
+        $compiledJade = $template.'.compiledJade.php';
+
+		if(file_exists($compiledJade))
+			return $compiledJade;
+
+        $compiled = file_exists($template) ? $renderer->render(file_get_contents($template))
+                                           : $renderer->render(file_get_contents($file));
+
+        if (is_writable($cachePath) && file_put_contents($compiledJade, $compiled) !== false && copy($file,$template))
+        {
+			foreach (glob("{$options['path']}/template_{$oname}_*.php") as $expired) {
+				if ( ! (bool) preg_match("@$template|$compiledJade@", $expired))
+					unlink($expired);
+			}
+			return $compiledJade;
+		}
+
+		throw new TemplateException("Could not write compiled template `{$template}` to cache.");
+
     }
+
+    /**
+	 * Returns a template file name
+	 *
+	 * @param string $type
+	 * @param array $params
+	 * @return string
+	 */
+	public function template($type, array $params) {
+		$params += array('library' => true);
+		$params['library'] = Libraries::get($params['library'], 'path');
+		return $this->_paths($type, $params);
+	}
 
 }
 
